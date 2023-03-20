8065fe4fade41709f8dd70bf951c959818ff2bfb
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-03-20 18:47:34 +0530

    save

--- src/component/validate/getUser.js
+++ src/component/validate/getUser.js
@@ -5,6 +5,7 @@ export function getCurrentUser() {
     const jwt = localStorage.getItem("token");
     const myDecodedToken = decodeToken(jwt);
     const isMyTokenExpired = isExpired(jwt);
+    if (isMyTokenExpired) localStorage.removeItem("token");
     return !isMyTokenExpired ? myDecodedToken.name : null;
   } catch (ex) {
     return null;
